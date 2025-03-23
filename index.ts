70b703c761fb45fe899981ec318ae8792747b157
yo-reign <danielaguiar0006@gmail.com>
2025-03-22 23:55:40 -0700

    Add mouse position tracking and scene re-drawing through `drawScene()`

--- index.ts
+++ index.ts
@@ -4,21 +4,20 @@ const GRID_COLS = 10;
 class Vector2 {
     constructor(public x: number, public y: number) { }
 
-    //*[Symbol.iterator]() {
-    //    yield this.x;
-    //    yield this.y;
-    //}
-
-    static add(a: Vector2, b: Vector2): Vector2 {
-        return new Vector2(a.x + b.x, a.y + b.y);
+    add(b: Vector2): Vector2 {
+        return new Vector2(this.x + b.x, this.y + b.y);
     }
 
-    static sub(a: Vector2, b: Vector2): Vector2 {
-        return new Vector2(a.x - b.x, a.y - b.y);
+    sub(b: Vector2): Vector2 {
+        return new Vector2(this.x - b.x, this.y - b.y);
     }
 
-    static scale(a: Vector2, b: number): Vector2 {
-        return new Vector2(a.x * b, a.y * b);
+    scale(b: Vector2): Vector2 {
+        return new Vector2(this.x * b.x, this.y * b.y);
+    }
+
+    div(b: Vector2): Vector2 {
+        return new Vector2(this.x / b.x, this.y / b.y);
     }
 }
 
@@ -37,6 +36,41 @@ function drawLine(ctx: CanvasRenderingContext2D, startPoint: Vector2, endPoint: 
     ctx.stroke();
 }
 
+function getCanvasSize(ctx: CanvasRenderingContext2D): Vector2 {
+    return new Vector2(ctx.canvas.width, ctx.canvas.height);
+}
+
+function drawScene(ctx: CanvasRenderingContext2D, p2: Vector2 | undefined) {
+    // Clear canvas (fill background)
+    ctx.reset();
+    ctx.fillStyle = "#202020";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    // Scale canvas from grid units to pixels
+    const cellWidthPx = ctx.canvas.width / GRID_COLS;
+    const cellHeight = ctx.canvas.height / GRID_ROWS;
+    ctx.scale(cellWidthPx, cellHeight);
+    ctx.lineWidth = 1 / cellWidthPx;
+
+    // Draw grid
+    ctx.strokeStyle = "#404040";
+    for (let x = 0; x <= GRID_ROWS; x++) {
+        drawLine(ctx, new Vector2(x, 0), new Vector2(x, GRID_ROWS), "#404040");
+    }
+    for (let y = 0; y <= GRID_COLS; y++) {
+        drawLine(ctx, new Vector2(0, y), new Vector2(GRID_COLS, y), "#404040");
+    }
+
+    // Draw points
+    var p1 = new Vector2(GRID_COLS / 2, GRID_ROWS / 2);
+    drawCircle(ctx, p1, 0.25, "#4400ff");
+
+    if (p2 !== undefined) {
+        drawCircle(ctx, p2, 0.25, "#4400ff");
+        drawLine(ctx, p1, p2, "#4400ff");
+    }
+}
+
 (() => {
     const gameCanvas = document.getElementById("game") as (HTMLCanvasElement | null);
     if (gameCanvas === null) {
@@ -50,32 +84,16 @@ function drawLine(ctx: CanvasRenderingContext2D, startPoint: Vector2, endPoint: 
         throw new Error("Unable to get `2d` context from canvas with id 'game'");
     }
 
-    // Clear canvas (fill background)
-    gameCtx.fillStyle = "#202020";
-    gameCtx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+    // p2 represents the mouse position in grid units
+    let p2: Vector2 | undefined = undefined;
+    gameCanvas.addEventListener("mousemove", (event) => {
+        p2 = new Vector2(event.offsetX, event.offsetY)
+            .div(getCanvasSize(gameCtx))
+            .scale(new Vector2(GRID_COLS, GRID_ROWS));
+        // Re-draw the scene with the updated p2 position
+        drawScene(gameCtx, p2);
+    });
 
-    // Scale canvas from grid units to pixels
-    const cellWidth = gameCanvas.width / GRID_COLS;
-    const cellHeight = gameCanvas.height / GRID_ROWS;
-    gameCtx.scale(cellWidth, cellHeight);
-    gameCtx.lineWidth = 1 / cellWidth;
-
-    // Draw grid
-    gameCtx.strokeStyle = "#404040";
-    for (let x = 0; x <= GRID_ROWS; x++) {
-        drawLine(gameCtx, new Vector2(x, 0), new Vector2(x, GRID_ROWS), "#404040");
-    }
-
-    for (let y = 0; y <= GRID_COLS; y++) {
-        drawLine(gameCtx, new Vector2(0, y), new Vector2(GRID_COLS, y), "#404040");
-    }
-
-    // Draw points
-    var p1 = new Vector2(GRID_COLS / 2, GRID_ROWS / 2);
-    drawCircle(gameCtx, p1, 0.25, "#4400ff");
-    var p2 = new Vector2(GRID_COLS / 4.45, GRID_ROWS / 4.25);
-    drawCircle(gameCtx, p2, 0.25, "#4400ff");
-
-    // Draw lines between points
-    drawLine(gameCtx, p1, p2, "#4400ff");
+    // Initial draw
+    drawScene(gameCtx, p2);
 })()
